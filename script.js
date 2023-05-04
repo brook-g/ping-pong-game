11d4aa00e345d67f4290d4cceb3f381a057dacc8
brook-g <broook@skiff.com>
2023-05-05 00:26:19 +0200

    replaced ball with noggles

--- script.js
+++ script.js
@@ -3,21 +3,20 @@ const ctx = canvas.getContext('2d');
 
 const paddleWidth = 10;
 const paddleHeight = 100;
-const ballRadius = 8;
+const characterWidth = 20;
+const characterHeight = 20;
 
 let paddle1Y = (canvas.height - paddleHeight) / 2;
 let paddle2Y = (canvas.height - paddleHeight) / 2;
-let ballX = canvas.width / 2;
-let ballY = canvas.height / 2;
-let ballSpeedX = 3;
-let ballSpeedY = 2;
+let characterX = canvas.width / 2;
+let characterY = canvas.height / 2;
+let characterSpeedX = 3;
+let characterSpeedY = 2;
 
-function drawBall() {
-  ctx.beginPath();
-  ctx.arc(ballX, ballY, ballRadius, 0, Math.PI * 2);
+function drawCharacter() {
+  ctx.font = '20px monospace';
   ctx.fillStyle = '#fff';
-  ctx.fill();
-  ctx.closePath();
+  ctx.fillText('⌐◨-◨', characterX, characterY);
 }
 
 function drawPaddle(x, y) {
@@ -31,29 +30,32 @@ function drawPaddle(x, y) {
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  ballX += ballSpeedX;
-  ballY += ballSpeedY;
+  characterX += characterSpeedX;
+  characterY += characterSpeedY;
 
-  if (ballX + ballRadius > canvas.width || ballX - ballRadius < 0) {
-    ballSpeedX = -ballSpeedX;
+  if (characterX + characterWidth > canvas.width || characterX < 0) {
+    characterSpeedX = -characterSpeedX;
   }
 
-  if (ballY + ballRadius > canvas.height || ballY - ballRadius < 0) {
-    ballSpeedY = -ballSpeedY;
+  if (characterY + characterHeight > canvas.height || characterY < 0) {
+    characterSpeedY = -characterSpeedY;
   }
 
-  if (ballX - ballRadius < paddleWidth && ballY > paddle1Y && ballY < paddle1Y + paddleHeight) {
-    ballSpeedX = -ballSpeedX;
+  if (characterX < paddleWidth && characterY > paddle1Y && characterY < paddle1Y + paddleHeight) {
+    characterSpeedX = -characterSpeedX;
   }
 
-  if (ballX + ballRadius > canvas.width - paddleWidth && ballY > paddle2Y && ballY < paddle2Y + paddleHeight) {
-    ballSpeedX = -ballSpeedX;
+  if (characterX + characterWidth > canvas.width - paddleWidth && characterY > paddle2Y && characterY < paddle2Y + paddleHeight) {
+    characterSpeedX = -characterSpeedX;
   }
 
-  paddle1Y = ballY - (paddleHeight / 2);
-  paddle2Y = ballY - (paddleHeight / 2);
+  paddle1Y = characterY - (paddleHeight / 2);
+  paddle2Y = characterY - (paddleHeight / 2);
 
-  drawBall();
+  drawCharacter();
   drawPaddle(0, paddle1Y);
-  drawPaddle(canvas.width
+  drawPaddle(canvas.width - paddleWidth, paddle2Y);
+}
+
+setInterval(draw, 10);
 
